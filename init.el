f5bbcbe46d9aac0d34b5474242eed797a96ff5f8
tinyblinker <2149934895@qq.com>
2026-08-06 00:28:15 +0800

    瘦身 init.el: 删除 add-lexical-binding-to-file 工具函数
    该函数是罕见使用的辅助工具(给文件添加 lexical-binding 声明头),
    不属于 init 启动流程,移除后将 init.el 精简到 16 行纯 bootstrap 脚本

--- init.el
+++ init.el
@@ -4,15 +4,6 @@
 (when (file-exists-p custom-file)
   (load custom-file nil :nomessage))
 
-(defun add-lexical-binding-to-file ()
-  (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (if (looking-at-p ".*lexical-binding.*")
-        (message "already had the 'lexical-binding'")
-      (insert ";;; -*- lexical-binding: t -*-\n\n")
-      (message "added successfully"))))
-
 (add-to-list 'load-path (expand-file-name "modules" user-emacs-directory))
 
 (require 'ui-config)
