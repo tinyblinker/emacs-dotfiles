f08798ccc0e853725cc71a9c077a611341dafa51
tinyblinker <2149934895@qq.com>
2026-08-12 14:19:48 +0800

    bugfix

--- modules/org-roam-config.el
+++ modules/org-roam-config.el
@@ -9,12 +9,13 @@
 (use-package org-roam
   :ensure t
   :defer t
-  :bind (("C-c n f" . org-roam-node-find)        ;; Find or create a note by title
-         ("C-c n i" . org-roam-node-insert)       ;; Insert a link to an existing note at point
-         ("C-c n l" . org-roam-buffer-toggle)    ;; Toggle the backlinks sidebar for the current note
-         ("C-c n g" . org-roam-graph)             ;; Show an interactive graph of note connections
-         ("C-c n t" . org-roam-tag-add)           ;; Add a tag to the current note
-         ("C-c n a" . org-roam-alias-add))        ;; Add an alias (alternate title) to the note
+  :bind (("C-c n f" . org-roam-node-find) ;; Find or create a note by title
+         ("C-c n i" . org-roam-node-insert) ;; Insert a link to an existing note at point
+         ("C-c n l" . org-roam-buffer-toggle) ;; Toggle the backlinks sidebar for the current note
+         ("C-c n g" . org-roam-graph) ;; Show an interactive graph of note connections
+         ("C-c n t" . org-roam-tag-add) ;; Add a tag to the current note
+         ("C-c n a" . org-roam-alias-add) ;; Add an alias (alternate title) to the note
+         ("C-M-i" . completion-at-point));;trigger the completion
   :custom
   ;; Store all Org-roam notes in the notes/ directory under the Emacs config.
   ;; file-truename resolves symlinks so the database stores canonical paths.
@@ -36,7 +37,9 @@
   (org-roam-db-autosync-mode)
   ;; Create the notes directory if it does not exist yet
   (unless (file-directory-p org-roam-directory)
-    (make-directory org-roam-directory t)))
+    (make-directory org-roam-directory t))
+  ;; setup the org-roam
+  (org-roam-setup))
 
 (provide 'org-roam-config)
 ;;; org-roam-config.el ends here
