3e9df3a10179e2705866a019f002421df9a4018f
shyweeds <2149934895@qq.com>
2026-09-14 11:06:26 +0800

    optimized requirement logic

--- init.el
+++ init.el
@@ -4,34 +4,11 @@
 (add-to-list 'load-path (expand-file-name "modules" user-emacs-directory))
 (require 'redirect-file-config)   ;; redirect unwanted files
 
-
-;; Buffers & files
-(require 'ibuffer-config)         ;; buffer list
-(require 'recentf-config)         ;; recent files
-(require 'savehist-config)        ;; history / kill ring
-(require 'dired-config)           ;; directory editor
-(require 'window-config)          ;; windows
-(require 'dirvish-config)         ;; file manager
-(require 'nerd-icons-config)      ;; icons
-
-;; LSP
-(require 'eglot-config)           ;; LSP client
-(require 'exec-path-from-shell-config) ;; shell env
-(require 'editorconfig-config)    ;; .editorconfig
-(require 'flymake-config)         ;; diagnostics navigation
-(require 'flymake-popon-config)   ;; diagnostics popup
-(require 'rust-config)            ;; cargo bindings
-
-;; Git & shell
-(require 'magit-config)           ;; git
-(require 'diff-hl-config)         ;; git indicators
-(require 'vterm-config)           ;; terminal
-
-;; Org
-(require 'org-config)             ;; org-mode
-(require 'ob-mermaid-config)      ;; mermaid in babel
-(require 'ox-gfm-config)          ;; GFM export
-(require 'org-roam-config)        ;; roam notes
+;; Load all other modules
+(dolist (file (directory-files (expand-file-name "modules" user-emacs-directory)
+                               t "\\.el\\'"))
+  (unless (equal (file-name-base file) "redirect-file-config")
+    (load file nil 'nomessage)))
 
 ;; Dark theme
 (load-theme 'modus-vivendi-tinted t)
